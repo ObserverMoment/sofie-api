94c1473625e22444d7b45d0711a0c93cbbb34756
ObserverMoment <bosonsoul@gmail.com>
2021-08-06 18:34:07 +0100

    getstream user updates

--- src/lib/getStream.ts
+++ src/lib/getStream.ts
@@ -1,4 +1,7 @@
+import { prisma, PrismaClient } from '@prisma/client'
 import { StreamChat } from 'stream-chat'
+import { UpdateUserInput } from '../generated/graphql'
+import { AccessScopeError } from '../graphql/utils'
 
 export let streamChatClient: StreamChat | null = null
 
@@ -34,6 +37,64 @@ export async function createStreamChatUser(userId: string) {
     console.log(e)
     throw new Error(e)
   }
+}
+
+export async function streamFieldsRequiringUpdate(
+  prisma: PrismaClient,
+  authedUserId: string,
+  data: UpdateUserInput,
+): Promise<string[]> {
+  const oldUser = await prisma.user.findUnique({
+    where: { id: authedUserId },
+    select: {
+      displayName: true,
+      avatarUri: true,
+    },
+  })
+
+  if (!oldUser) {
+    throw new AccessScopeError(
+      'streamFieldsRequiringUpdate: Unable to find object to check',
+    )
+  } else {
+    let fieldsRequiringUpdate = []
+
+    if (
+      data.hasOwnProperty('displayName') &&
+      data['displayName'] !== oldUser.displayName
+    ) {
+      fieldsRequiringUpdate.push('displayName')
+    }
+    if (
+      data.hasOwnProperty('avatarUri') &&
+      data['avatarUri'] !== oldUser.avatarUri
+    ) {
+      fieldsRequiringUpdate.push('avatarUri')
+    }
+
+    return fieldsRequiringUpdate
+  }
+}
+
+/// If the user updates their displayName or their avatarUri then we need to update the associated user on getStream. Their ID on getStream will match the ID is the database.
+export async function updateGetStreamFields(
+  authedUserId: string,
+  fieldsToUpdate: string[],
+  data: any, // UpdateUserInput - cast as any so can index in by string.
+) {
+  if (!streamChatClient) {
+    throw Error('streamChatClient not initialized')
+  }
+
+  const obj = fieldsToUpdate.reduce((acum, next) => {
+    acum[next] = data[next]
+    return acum
+  }, {} as any)
+
+  await streamChatClient!.upsertUser({
+    id: authedUserId,
+    ...obj,
+  })
 }
 
 /**
